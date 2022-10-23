a1c40299e28e6830377f24300244c225e2552ee8
Konoha211 <lcr472752653@gmail.com>
2022-10-23 01:21:24 -0400

    final

--- js/site.js
+++ js/site.js
@@ -1,7 +1,27 @@
-let thathazyfeeling = {
+ document.getElementById("a").addEventListener("click", myFunction1);
 
+function myFunction1() {
+    document.body.style.backgroundColor = "orangered";
+    document.getElementById("myImg").src = "image/2.jpg";
+}
 
+document.getElementById("b").addEventListener("click", myFunction2);
 
+function myFunction2() {
+    document.body.style.backgroundColor = "slategray";
+    document.getElementById("myImg").src = "image/3.jpg";
+}
 
-    
+document.getElementById("c").addEventListener("click", myFunction3);
+
+function myFunction3() {
+    document.body.style.backgroundColor = "coral";
+    document.getElementById("myImg").src = "image/4.png";
+}
+
+document.getElementById("d").addEventListener("click", myFunction4);
+
+function myFunction4() {
+    document.body.style.backgroundColor = "white";
+    document.getElementById("myImg").src = "image/1.jpg";
 }
